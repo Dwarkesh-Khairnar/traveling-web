93daefa80b218bfb9812c3f58d5d60aa1b1432e9
Khairnar Dwarkesh <143956295+Dwarkesh-Khairnar@users.noreply.github.com>
2025-03-09 21:23:56 +0530

    Update app.js
    #

--- public/js/app.js
+++ public/js/app.js
@@ -78,7 +78,7 @@ if (window.location.pathname === '/gallery') {
   // //         form.addEventListener('submit', (e) => {
   // //             e.preventDefault();
   // //             const title = document.getElementById('title').value;
-  // //             const content = document.getElementById('content').value;
+  //            const content = document.getElementById('content').value;
 
   //            fetch('/blog', {
   //                 method: 'POST',
