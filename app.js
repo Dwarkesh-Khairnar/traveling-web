dd5b78b7cdcc947cd4fc281efcb92253185b2851
Khairnar Dwarkesh <143956295+Dwarkesh-Khairnar@users.noreply.github.com>
2025-08-03 23:29:38 +0530

    Update app.js
    #

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const router = express.Router();
 const cors =require('cors')
 const PORT = process.env.PORT || 3000;
 
-
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -88,3 +87,4 @@ db.connect(err => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
