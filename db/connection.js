2ffd33ae3a62f945794fbc5438ff58825fc1a3c5
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2025-10-04 21:39:53 +0530

    connection update

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
@@ -8,17 +8,3 @@ const connection = mysql.createConnection({
     database: 'traveldb'
   });
   module.exports = connection;
-
-=======
-const mysql = require('mysql2');
-
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'traveldb'
-});
-// .promise()
-  module.exports = connection;
->>>>>>> c3825673c631771d9ef7e28377b90e23a36082dd
