896d016336c13543a13673f5a61834d26ddf5a23
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2026-02-12 22:31:21 +0530

    Delete unuse gallery endpoints code add routes

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 // Route Files
 import blogsRoute from './routes/Blogs.js'
+import galleryRoute from './routes/Gallery.js'
 
 // Middleware
 import requireLogin from "./middleware/session_auth.js";
@@ -163,17 +164,6 @@ const transporter = nodemailer.createTransport(
 // }
 
 
-//  Gallery Route
-app.get('/api/gallery', (req, res) => {
-    const query = 'SELECT * FROM places';
-    db.query(query, (err, results) => {
-        if (err) {
-            console.error('Database query error:', err);
-            return res.status(500).json({ error: 'Failed to fetch places' });
-        }
-        res.json(results); // Send photos posts to the front-end
-    });
-});
 
 
 let lat, lng, finelsearch;
@@ -195,6 +185,7 @@ app.get('/api/find', (req, res) => {
 })
 
 app.use('/api', blogsRoute);
+app.use('/api', galleryRoute);
 
 
 // blogs finders code
@@ -507,52 +498,6 @@ app.post('/api/users/delete', (req, res) => {
         res.redirect('/bord')
     });
 });
-
-// gallery data
-app.post('/api/gallery/add', (req, res) => {
-    const { title, image_url, caption } = req.body;
-    db.query('insert into places(name,image_url,description) values(?,?,?)', [title, image_url, caption], (err) => {
-        if (err) throw err;
-        res.json(results)
-    });
-});
-
-app.post('/api/gallery/delete', (req, res) => {
-    const { id } = req.body;
-    db.query('DELETE FROM places where id=?', [id], err => {
-        if (err) throw err;
-        res.redirect('/bord')
-    });
-});
-
-
-
-
-// User gallery Route
-app.get('/api/user/gallery', (req, res) => {
-    const query = `SELECT * FROM places where mail=?`;
-    db.query(query, [usermail], (err, results) => {
-        if (err) {
-            console.error('Database query error:', err);
-            return res.status(500).json({ error: 'Failed to fetch places' });
-        }
-        res.json(results); // Send photos posts to the front-end
-    });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.get('/logout', (req, res) => {
     req.session.destroy((err) => {
